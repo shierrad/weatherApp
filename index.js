4621c5ec6f93b25e50e5b7a4e71c16f7a2056abf
shierrad <shierra@ciencias.unam.mx>
2025-08-06 12:24:44 -0600

    remove package-lock

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+
+// get api key from external file
 import userData from "./userData.json" assert { type: 'json' };
 
 const app = express();
 const port = 3000;
-const GEO_API_URL = "http://api.openweathermap.org/geo/1.0";
-const WEA_API_URL = "https://api.openweathermap.org/data/3.0";
-const apiKey = userData.apiKey;
+const WEA_API_URL = "https://api.openweathermap.org/data/2.5";
 
+// get api key from file, change this to your own api key
+const apiKey = userData.apiKeys[1];
 
 app.use(express.static("public"));
 console.log(apiKey);
 
-/*
 app.get("/", async (req, res) =>{
     const city = "London";
-    const GEO_URL = `${GEO_API_URL}/direct?q=${city}&appid=${apiKey}`
+    const WEA_URL = `${WEA_API_URL}/forecast?q=${city}&units=metric&appid=${apiKey}`
     try {
-        const result = await
+        const result = await axios.get(WEA_URL);
+        console.log(result.data.list[0]);
+    } catch (error) {
+        console.log(error);
     }
-}) */
+}) 
 
 
 app.listen(port, () =>{
