53f29be1b407c2ed2d1900e2341ca2ba38d4f780
shierrad <shierra@ciencias.unam.mx>
2025-08-06 09:57:32 -0600

    create file structure

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const port = 3000;
+const wAPI_URL = "";
+
+
+
+
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})
