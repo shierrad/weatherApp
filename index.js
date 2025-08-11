6dc2bef9925771cd36e565d460b6a69cc02e1e98
shierrad <shierra@ciencias.unam.mx>
2025-08-11 12:01:02 -0600

    add post method to template, add current weather and forecasts

--- index.js
+++ index.js
@@ -13,20 +13,44 @@ const WEA_API_URL = "https://api.openweathermap.org/data/2.5";
 const apiKey = userData.apiKeys[1];
 
 app.use(express.static("public"));
-console.log(apiKey);
+app.use(express.urlencoded({extended: true}));
 
-app.get("/", async (req, res) =>{
-    const city = "London";
-    const WEA_URL = `${WEA_API_URL}/forecast?q=${city}&units=metric&appid=${apiKey}`
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {content: "Search for your city"});
+});
+
+
+app.post("/weather", async (req, res) =>{
+    const city = req.body.city;
+    const FOR_URL = `${WEA_API_URL}/forecast?q=${city}&units=metric&appid=${apiKey}`
+    const WEA_URL = `${WEA_API_URL}/weather?q=${city}&units=metric&appid=${apiKey}`
     try {
-        const result = await axios.get(WEA_URL);
-        console.log(result.data.list[0]);
+        const forecastResult = await axios.get(FOR_URL);
+        const weatherResult = await axios.get(WEA_URL);
+        const [currWea, currTemp, currFeels, currIcon, place] = getCurrentInfo(weatherResult.data)
+        console.log(currWea, currTemp, currFeels, currIcon, place)
+        const forecasts = forecastResult.data.list;
+        //const message = `${forecasts[0].weather[0].description} \n in ${result.data.city.name}, ${result.data.city.country}.`;
+        //res.render("index.ejs", {content: message});
+        //console.log(forecasts[0], forecasts[1]);
+        console.log(weatherResult.data);
     } catch (error) {
         console.log(error);
-    }
+    } 
 }) 
 
 
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
 })
+
+
+function getCurrentInfo(weatherData){
+    const currWea = weatherData.weather[0].description;
+    const currIcon = weatherData.weather[0].icon;
+    const currTemp = weatherData.main.temp;
+    const currFeels = weatherData.main.feels_like;
+    const place = `${weatherData.name}, ${weatherData.sys.country}`;
+    return [currWea, currTemp, currFeels, currIcon, place];
+}
